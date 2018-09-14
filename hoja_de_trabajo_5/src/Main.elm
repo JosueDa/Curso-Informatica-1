56bf518b63a3d2ad8ec45ef55856224edb0eb437
JosueDa <41441611+JosueDa@users.noreply.github.com>
2018-09-13 22:02:35 -0600

    Add files via upload

--- hoja_de_trabajo_5/src/Main.elm
+++ hoja_de_trabajo_5/src/Main.elm
@@ -1,39 +1,33 @@
 module Main exposing (..)
 
-suma: Int -> Int
-suma x = x + x
+esPrimo: Int -> Bool
+esPrimo n =  primoN 2 n 
+primoN  cont n =
+    if n == 1 
+    then False
+    else if n == 2 
+    then True 
+    else if modBy   cont  n  == 0 
+    then False 
+    else if cont == n-1
+    then True
+    else primoN (cont + 1) n
 
-duplicar: Int -> Int
-duplicar z = z * z
+fibonacci : Int-> Int
+fibonacci n = 
+    if n == 0 then 0
+    else if n == 1 then 1
+    else if n > 1 then fibonacci (n-1) + fibonacci (n-2) else 0
 
-zip : List a -> List b -> List (a,b)
-zip bss css =
-    case (bss,css) of 
-        ([],_) -> []
-        (_,[]) -> []
-        (a::bs, b::cs) -> (a,b)::zip bs cs
+primos : Int -> List Int
+primos n =
+    if 1 > n then []
+    else if esPrimo n == False then primos (n-1)
+    else n :: primos (n-1) 
 
-type Natural = Suc Natural | Cero
-enteroANatural : Int -> Natural
-enteroANatural i = if i == 0 then Cero else Suc (enteroANatural (i-1))
-
-sumar n1 n2 = case (n1,n2) of 
-    (Cero, n2_) -> n2_
-    (n1_, Cero) -> n1_
-    (Suc n1_, n2_) -> Suc (sumar n1_ n2_ )
-
-type Figura = Circulo Float
-    | Cuadrado Float
-    | Triangulo Float
-
-area : Figura -> Float
-area figura = case figura of 
-    Circulo r -> Basics.pi * r * r
-    Cuadrado l -> l*l
-    Triangulo l1 l2 -> l1 * (sqrt ((l1/2)*(l1/2) + l2*l2))
-
-type MiList a = Cons a (MiList  a) | Nil
-    crearLista : [a] -> MiList a
-    crearLista l = case l of
-        [] -> Nil
-        x::xs -> Cons x (crearLista xs)
+nprimos: Int -> List Int
+nprimos n  = incluye (n,2) 
+incluye (n,x) = 
+    if n == 0 then [] 
+    else if esPrimo x == False then incluye (n, x + 1)
+    else x:: incluye (n - 1, x + 1)
