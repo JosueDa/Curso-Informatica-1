cad2b1838966b46c2a7b107e1ebaade012a45bf9
JosueDa <41441611+JosueDa@users.noreply.github.com>
2018-09-27 23:44:03 -0600

    Add files via upload

--- Hoja de trabajo 6/src/Main.elm
+++ Hoja de trabajo 6/src/Main.elm
@@ -1,19 +1,38 @@
-module Hoja7 exposing (..)
+module Hoja6 exposing (..)
 
-suma : Int -> Int -> Int
-suma x y = x + y
+type Natural = Suc Natural | Cero
 
-multiplicacion : Int -> Int -> Int
-multiplicacion x y = x * y
+resta : Natural -> Natural -> Natural
+resta x y = case (x,y) of 
+    (x_, Cero) -> x
+    (Cero, y_) -> Cero
+    (Suc x_, Suc y_) -> resta x_ y_
 
-type Expresion = Valor Int
-    | Suma Expresion Expresion 
-    | Multiplicacion Expresion Expresion  
-    
+suma : Natural -> Natural -> Natural
+suma x y = case (x, y) of 
+    (x_, Cero) -> x_
+    (Cero, y_) -> y_
+    (Suc x_, Suc y_) ->  Suc (Suc (suma x_ y_))
 
-reducir : ((Int -> Int -> Int), (Int-> Int -> Int)) -> Expresion -> Int
-reducir (adicion, multi) exp = case exp of
-    Valor x -> x
-    Suma op1 op2 -> adicion (reducir (adicion, multi) op1)  (reducir (adicion, multi) op2)
-    Multiplicacion op1 op2 -> multi (reducir (adicion, multi) op1)  (reducir (adicion, multi) op2)
-    
+multiplicacion : Natural -> Natural -> Natural
+multiplicacion x y = case (x, y) of
+    (x_, Cero) -> Cero
+    (Cero, y_) -> Cero
+    (Suc x_, y_) -> suma y_ (multiplicacion x_ y_)
+
+
+division : Natural -> Natural -> (Natural, Natural)
+division x y = count (x) (y) (Cero)
+count x y z = 
+    if x == (Suc(Cero)) && y == (Suc (Cero)) then ((Suc(z), Cero))
+    else if resta x y == Cero then (z, y)
+    else count (resta x y) (y) (Suc (z))
+
+type GExpresion x = Valor x 
+    | Suma (GExpresion x) (GExpresion x)
+    | Multiplicacion (GExpresion x) (GExpresion x)
+
+type alias Expresion = GExpresion Int
+
+type Estado = Final Int 
+    | Parcial (List Char)
